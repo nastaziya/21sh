e99f4ad7b76c334b464c5d0b28206cc25117cef2
Guillaume <guillaume.derouville@gmail.com>
2018-10-07 12:45:02 +0200

    correct management of the alt down key

--- src/termcaps/termcap.c
+++ src/termcaps/termcap.c
@@ -458,13 +458,26 @@ int			alt_down_key(t_tcap *caps)
 	
 	size_windows(caps);
 	cursor_position(curs_pos);
-	if (curs_pos[1] < ((caps->y_prompt) + (caps->sz_str / (caps->window_size[1])))) // && curs_pos[1] < ((caps->y_prompt) + (caps->sz_str / (caps->window_size[1] - 1)))
+	position_char_in_window_left_alt_keys(caps->cursor, caps, curs_pos);
+	if (caps->x_lines[2] != -1) // && curs_pos[1] < ((caps->y_prompt) + (caps->sz_str / (caps->window_size[1] - 1)))
 	{
+		dprintf(2, "miaouh");
+		position_char_in_window_print_inside_string(caps->cursor, caps, caps->sz_str);
 		// dprintf(2, "curs_pos[1]: %d, calc_complet_y: [%d], calc_alt_down: [%d]\n", curs_pos[1], ((caps->y_prompt) + (caps->sz_str / (caps->window_size[1] - 1))), (caps->sz_str % (caps->window_size[1])));
-		if (curs_pos[1] == ((caps->y_prompt) + (caps->sz_str / (caps->window_size[1])) - 1) && curs_pos[0] > (caps->sz_str % (caps->window_size[1]))) //curs_pos[0] >= (caps->sz_str % (caps->window_size[1])) && 
+		if (curs_pos[1] == caps->char_pos[1] - 1 && curs_pos[0] > caps->x_lines[2]) //curs_pos[0] >= (caps->sz_str % (caps->window_size[1])) && 
 		{
 			dprintf(2, "ici mon char");
 			end_key(caps);
+		}
+		else if (curs_pos[0] > caps->x_lines[2])
+		{
+			tputs(tgetstr("vi", NULL), 1, ft_outc);
+			int i = -1;
+			while (++i < (caps->x_lines[1] - curs_pos[0]) + caps->x_lines[2] + 2)// + 1
+				right_key(caps);
+			// tputs(tgetstr("up", NULL), 1, ft_outc);
+			// caps->cursor = caps->cursor - caps->window_size[1];
+			tputs(tgetstr("ve", NULL), 1, ft_outc);
 		}
 		else
 		{
@@ -472,7 +485,7 @@ int			alt_down_key(t_tcap *caps)
 			tputs(tgetstr("vi", NULL), 1, ft_outc);
 			// tputs(tgoto(tgetstr("cm", NULL), curs_pos[0] - 1, curs_pos[1]), 1, ft_outc);
 			int i = -1;
-			while (++i < caps->window_size[1])
+			while (++i < (caps->x_lines[1] - curs_pos[0]) + curs_pos[0] + 1)
 				right_key(caps);
 			// caps->cursor = caps->cursor + caps->window_size[1];
 			tputs(tgetstr("ve", NULL), 1, ft_outc);
